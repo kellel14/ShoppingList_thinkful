2bd2bf711b947a9412e9b5f2065900e1cfe380a5
System <none>
2017-05-25 03:54:31 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -6,14 +6,17 @@ Click `Show` in the header to see your app live. Updates to your code will insta
 Project Overview
 ------------
 
-For this project,
+For this project:
 - you will mainly be working inside `public/client.js`
 - you will also work inside `views/index.html` and `public/style.css`
 - you can drag in `assets` such as images to add them to your project
 - you can ignore all the other files :)
 
-Things to Google off the bat
-  document.ready
+
+Things you might want to Google:
+- your Javascript code is inside `$(document).ready(function() { })`. Why?
+- 
+
 
 Step 1: Adding an item
 - edit `public/client.js`
--- public/client.js
+++ public/client.js
@@ -1,26 +1,37 @@
-// client-side js
-// run by the browser each time your view template is loaded
+$(document).ready(function() {
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
+// Step 1
+// -------------------
+	$(".add-item").on('click', function(event) {
+		var listItem = $(".item-input").val();
+			var itemHtml = "<li class='items'><span class='item-check incomplete'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'><img src='images/remove.png' alt='remove' width='20px'></span></li>";
+			if(listItem.length > 0) {
+				$(".shopping-list").prepend(itemHtml);
+				$(".item-input").val("");
+			}
+	});
+// -------------------
 
-$(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
+// Step 2
+// -------------------
+	$(".shopping-list").on('click', '.item-check', function(event) {
+		if($(this).hasClass( "incomplete" )) {
+			$(this).removeClass("incomplete");
+			$(this).addClass("complete");
+		} else {
+			$(this).removeClass("complete");
+			$(this).addClass("incomplete");
+		}
+	});
+// -------------------
+
+
+// Step 3
+// -------------------
+	$(".shopping-list").on('click', '.item-remove', function(event) {
+		$(this).parent().remove();
+	});
+// -------------------
 
 });
