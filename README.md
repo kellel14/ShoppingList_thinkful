5560e0feb4715eff0126193980c73708f900021b
System <none>
2017-06-05 17:15:08 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -23,13 +23,13 @@ Challenges
 - In `Step 1` we've added an "event listener" which runs the code between the curly brackets when `Add Item` is clicked
 - We've given you the HTML you'll add to the `shopping-list` section when the button is clicked. That HTML is stored in the variable `itemHtml`
 
-**Step 2: Making an item inactive (when someone clicks the checkbox)**
-- In `Step 2` we've added an "event listener" which runs the code between the curly brackets when the checkbox is clicked
-- The shopping item will become inactive when the `complete` class is added 
-
-**Step 3: Removing an item (when someone clicks `X`)**
+**Step 2: Removing an item (when someone clicks `X`)**
 - In `Step 3` we've added an "event listener" which runs the code between the curly brackets when the "X" is clicked
 - When you click the "X", the shopping item should be removed (hint, you'll need to use "traversing")
+
+**Step 3: Making an item inactive (when someone clicks the checkbox)**
+- In `Step 2` we've added an "event listener" which runs the code between the curly brackets when the checkbox is clicked
+- The shopping item will become inactive when the `complete` class is added 
 
 **More jQuery Challenges**
 - If you add an item with no text, it adds a blank box. This is a bug! Fix it.
--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 // -------------------
 	$(".add-item").on('click', function(event) {
 		  var listItem = $(".item-input").val();
-		  var itemHtml = "<li class='items'><span class='item-check incomplete'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'><img src='https://cdn.glitch.com/72548e86-2a07-45d1-9756-a034ea6672b3%2Fremove.png?1495684443965' alt='remove' width='20px'></span></li>";
+		  var itemHtml = "<li class='items'><span class='item-check'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'><img src='https://cdn.glitch.com/72548e86-2a07-45d1-9756-a034ea6672b3%2Fremove.png?1495684443965' alt='remove' width='20px'></span></li>";
       // add your code here
 	});
 // -------------------
@@ -12,15 +12,14 @@ $(document).ready(function() {
 
 // Step 2
 // -------------------
-	$(".shopping-list").on('click', '.item-check', function(event) {
+	$(".shopping-list").on('click', '.item-remove', function(event) {
       // add your code here
 	});
 // -------------------
-
-
+  
 // Step 3
 // -------------------
-	$(".shopping-list").on('click', '.item-remove', function(event) {
+	$(".shopping-list").on('click', '.item-check', function(event) {
       // add your code here
 	});
 // -------------------
